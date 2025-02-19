e7686a47dc41a2f9bb989c448c566fbb0e2f2081
BluLie7 <ivan.deszmo1@gmail.com>
2025-02-19 17:42:53 +0100

    todo list

---
+++ todo-func.js
@@ -0,0 +1,58 @@
+
+let todolist = JSON.parse(localStorage.getItem('todo-List')) || [];
+listRead();
+minDate();
+function listRead() {
+
+    let todoListHTML = '';
+    for (let i = 0; i < todolist.length; i++) {
+        const todoObject = todolist[i];
+        const name = todoObject.name;
+        const duedate = todoObject.dueDate;
+        const html = `<div>${i + 1}. ${name} </div>
+                    <div>${duedate}</div>  
+                    <button onclick="deleteFromList(${i});">Delete</button>`;
+        todoListHTML += html;
+    }
+
+
+
+
+    document.querySelector('.list-elements').innerHTML = todoListHTML;
+    localStorage.setItem('todo-List', JSON.stringify(todolist));
+
+}
+
+function deleteFromList(index) {
+    todolist.splice(index, 1);
+    localStorage.setItem('todo-List', JSON.stringify(todolist));
+    listRead();
+
+}
+
+function minDate() {
+    const today = new Date().toISOString().split('T')[0];
+    document.querySelector('.date-input').min = today;
+}
+
+function addToList() {
+    const newtask = document.querySelector('.task-input');
+    const newDueDate = document.querySelector('.date-input');
+    const name = newtask.value;
+    const dueDate = newDueDate.value;
+    if (newtask.value.length > 0) {
+        todolist.push({
+            name: name,
+            dueDate: dueDate
+        });
+    }
+    localStorage.setItem('todo-List', JSON.stringify(todolist));
+    newtask.value = '';
+    listRead();
+}
+
+function enterValue(event) {
+    if (event.key === 'Enter') {
+        addToList();
+    }
+}
